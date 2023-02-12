bb07a8bb9f8516011a8b521ad21e3a1f98b9f291
ike <ike0738@ikeda-naobun-no-MacBook-Pro-10.local>
2023-02-12 12:11:35 +0900

    馬たちのページ始動

--- js/horse_0001_ttr.js
+++ js/horse_0001_ttr.js
@@ -1,15 +1,15 @@
-var ctx = document.getElementById('horse_0001_sample').getContext('2d');
+var ctx = document.getElementById('horse_0001_ttr').getContext('2d');
 var myChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ['2022.11.01','2022.11.02','2022.11.03','2022.11.04','2022.11.05','2022.11.06','2022.11.07','2022.11.08','2022.11.09','2022.11.10','2022.11.11','2022.11.12','2022.11.13','2022.11.14',''],
+    labels: ['2023.02.08','2023.02.09','2023.02.10','2023.02.11',''],
     datasets: [{
       label: '体重',
-      data: [450,451,452,453,454,453,452,451,452,453,454,455,456,458,],
+      data: [402,397,399,395,],
       borderColor: '#333',
     }, {
       label: '体温',
-      data: [37.5, 37.3, 37.0, 37.3, 37.2, 37.5, 37.8, 37.3, 37.0, 37.3, 37.2, 37.5, 37.8, 37.8, ],
+      data: [37.5, 37.3, 37.3, 37.1, ],
       borderColor: '#f88',
       yAxisID: 'y2',
     }],
@@ -17,8 +17,8 @@ var myChart = new Chart(ctx, {
   options: {
     scales: {
       y: {   // 体重
-        min: 400,
-        max: 500,
+        min: 350,
+        max: 550,
         ticks: {
           color: '#333',
           stepSize: 25,
--- js/horse_0002_glm.js
+++ js/horse_0002_glm.js
@@ -1,15 +1,15 @@
-var ctx = document.getElementById('horse_0002_sample').getContext('2d');
+var ctx = document.getElementById('horse_0002_glm').getContext('2d');
 var myChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ['2022.11.01','2022.11.02','2022.11.03','2022.11.04','2022.11.05','2022.11.06','2022.11.07','2022.11.08','2022.11.09','2022.11.10','2022.11.11','2022.11.12','2022.11.13',''],
+    labels: ['2023.02.08','2023.02.09','2023.02.10','2023.02.11',''],
     datasets: [{
       label: '体重',
-      data: [450,451,459,458,457,457,450,451,459,458,457,457,450,],
+      data: [478,478,478,477,],
       borderColor: '#333',
     }, {
       label: '体温',
-      data: [36.5, 36.3, 36.0, 36.3, 36.2, 36.5, 36.8, 36.3, 36.0, 36.3, 36.2, 36.5, 36.8, ],
+      data: [37.3, 37.3, 37.4, 37.2, ],
       borderColor: '#f88',
       yAxisID: 'y2',
     }],
@@ -17,8 +17,8 @@ var myChart = new Chart(ctx, {
   options: {
     scales: {
       y: {   // 体重
-        min: 400,
-        max: 500,
+        min: 350,
+        max: 550,
         ticks: {
           color: '#333',
           stepSize: 25,
